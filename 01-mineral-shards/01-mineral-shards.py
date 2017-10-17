b74b0f19b576cae571a995642411b523c89e15af
Jack Chen <jackchen@wildhorn.ece.pdx.edu>
2017-10-17 16:22:27 -0700

    Framework of work to be done for the agent

--- 01-mineral-shards/01-mineral-shards.py
+++ 01-mineral-shards/01-mineral-shards.py
@@ -1,1 +1,10 @@
+import numpy as np
+import os
+import tensorflow as tf
 
+# Load env
+# Create DQN model with baselines
+# Create replay buffer with baselines' built-in function
+# Get action from DQN and map to action that is in ['available_actions']
+# Execute action, save (s, a, r, s_) to memory
+# (?) Something about shifting a matrix when army moves
